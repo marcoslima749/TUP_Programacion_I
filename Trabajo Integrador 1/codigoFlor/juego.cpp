40cc4d0783e0a8626f8c4b0e39fc400405a85e77
Marcos Lima <marcoslima749@gmail.com>
2024-10-28 21:10:59 -0300

    Cambia tirarDado->tirarUnDado|tiradaDeDados->tirarNDados|mostratPuntajeDeRonda->mostrarPuntajeDeRonda|bandera->seguirTirando|can->dadosDisponibles, y ordena un poco el codigo segun reunion sabado 26/10

--- Trabajo Integrador 1/codigoFlor/juego.cpp
+++ Trabajo Integrador 1/codigoFlor/juego.cpp
@@ -5,14 +5,14 @@
 
 using namespace std;
 
-int tirarDado();
-void tiradaDeDados(int vDados[], int cantidad);
+int tirarUnDado();
+void tirarNDados(int vDados[], int cantidad);
 void mostrarDados(int vDados[], int cantidad);
 int calcularPuntaje(int vDados[], int dadosBloqueadores[], int &cantidad);
 int jugar(int puntaje,string nombre, int resultados[],int totalrondas);
 string pedirNombre(string &name);
 void mostrarNombre(string &name);
-void mostratPuntajeDeRonda(int vec[],int cantidad);
+void mostrarPuntajeDeRonda(int vec[],int cantidad);
 void puntajeTotal(int vec[],int cantidad);
 void finDelaPartida(int dadosPuntajes[],int cantidadDeRondas, int cantidadDeDados);
 bool sonTodosIguales(int vec[],int tam);
@@ -20,114 +20,96 @@ void puntajeparcial(int lista1[],int lista2[], int cantidad,string nombre1,strin
 
 //------------------------------------------------------------------------------------------------------------------------
 
-int main()
-{
+int main(){
 
-  srand(time(0));
-int modoJugador,puntaje;
-string nombre;
+    srand(time(0));
+    int modoJugador,puntaje;
+    string nombre;
 
-cout<<"Ingrese modo de juego \n"<<"1 JUGADOR: PRESIONE 1 \n"<<"2 JUGADORES: PRESIONE 2"<<endl;
-cin>>modoJugador;
+    cout<<"Ingrese modo de juego \n"<<"1 JUGADOR: PRESIONE 1 \n"<<"2 JUGADORES: PRESIONE 2"<<endl;
+    cin>>modoJugador;
 
-//MODO 1 JUGADOR
+    //MODO 1 JUGADOR
 
-if (modoJugador==1){
+    if (modoJugador==1){
 
-int puntos[3]{},rondas=1,i;
+        int puntos[3]{},rondas=1,i;
 
-nombre=pedirNombre(nombre);
+        nombre=pedirNombre(nombre);
 
-for (i=0;i<3;i++){
+        for (i=0;i<3;i++){
 
-  int pausa=0;
+            int pausa=0;
 
-   puntos[rondas-1]=jugar(puntaje,nombre,puntos,rondas);
-   rondas = rondas + 1;
-   puntajeparcial(puntos,0,3,nombre,"0");
-   cout<<"Presione 1 para continuar"<<endl;
-   cin>> pausa;
+            puntos[rondas-1]=jugar(puntaje,nombre,puntos,rondas);
+            rondas = rondas + 1;
+            puntajeparcial(puntos,0,3,nombre,"0");
+            cout<<"Presione 1 para continuar"<<endl;
+            cin>> pausa;
 
+        }
 
-}
+    } else { //MODO 2 JUGADORES
 
- }
+        string jugador1, jugador2;
 
- //MODO 2 JUGADORES
- else{
-
-string jugador1, jugador2;
-
-int rondas=1,i ;
-int puntos1,listapuntos1[3]{};
-int puntos2,listapuntos2[3]{};
+        int rondas=1,i ;
+        int puntos1,listapuntos1[3]{};
+        int puntos2,listapuntos2[3]{};
 
 
-jugador1=pedirNombre(jugador1);
-jugador2=pedirNombre(jugador2);
+        jugador1=pedirNombre(jugador1);
+        jugador2=pedirNombre(jugador2);
 
-for (i=0;i<3;i++){
- int pausa=0;
+        for (i=0;i<3;i++){
 
+            int pausa=0;
 
- //JUGADOR 1
- listapuntos1[rondas-1]=jugar(puntaje,jugador1,listapuntos1,rondas);
- puntajeparcial(listapuntos1,listapuntos2,3,jugador1,jugador2);
+            //JUGADOR 1
+            listapuntos1[rondas-1]=jugar(puntaje,jugador1,listapuntos1,rondas);
+            puntajeparcial(listapuntos1,listapuntos2,3,jugador1,jugador2);
 
- cout<<"Presione 1 para continuar"<<endl;
- cin>> pausa;
+            cout<<"Presione 1 para continuar"<<endl;
+            cin>> pausa;
 
+            //JUGADOR 2
+            listapuntos2[rondas-1]=jugar(puntaje,jugador2,listapuntos2,rondas);
 
+            puntajeparcial(listapuntos1,listapuntos2,3,jugador1,jugador2);
 
-//JUGADOR 2
- listapuntos2[rondas-1]=jugar(puntaje,jugador2,listapuntos2,rondas);
+            cout<<"Presione 1 para continuar"<<endl;
+            cin>> pausa;
+            rondas = rondas + 1;
 
- puntajeparcial(listapuntos1,listapuntos2,3,jugador1,jugador2);
-
- cout<<"Presione 1 para continuar"<<endl;
- cin>> pausa;
- rondas = rondas + 1;
-
-
-
- }
- }
-
-
-
-
-
-
-
-
+        }
+    }
 
   return 0;
+
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-int tirarDado()
-{
-  return rand() % 6 + 1;
+int tirarUnDado(){
+    return rand() % 6 + 1;
 }
 
-void tiradaDeDados(int vDados[], int cantidad)
-{
-  for(int i=0; i<cantidad; i++)
-  {
-    vDados[i] = tirarDado();
-  }
+void tirarNDados(int vDados[], int cantidad){
+
+    for(int i=0; i<cantidad; i++){
+        vDados[i] = tirarUnDado();
+    }
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 void mostrarDados(int vDados[], int cantidad){
-  for(int i=0; i<cantidad; i++)
-  {
-    cout << " " << vDados[i];
-  }
+    for(int i = 0; i < cantidad; i++){
+        cout << " " << vDados[i];
+    }
 
-  cout << endl;
+    cout << endl;
+
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -157,87 +139,86 @@ int calcularPuntaje(int vDados[], int dadosBloqueadores[], int &cantidad){
 
 int jugar(int puntaje,string nombre,int resultado[],int totalrondas) {
 
-int dadosJuego[5],dadosBloqueadores[2],tirada=0,can=5,i,rondas=0,bandera=true;
+    int dadosJuego[5],dadosBloqueadores[2],tirada=0,dadosDisponibles=5,i,rondas=0,seguirTirando=true;
 
 
-system("cls");
+    system("cls");
+
+    rondas = rondas + 1;
+    tirada = 0;
+    dadosDisponibles = 5;
+    seguirTirando = true;
 
 
+    tirarNDados(dadosBloqueadores,2);
 
 
- rondas = rondas + 1;
- tirada = 0;
- can = 5;
- bandera = true;
+    while( dadosDisponibles > 0 && seguirTirando ){
 
+        mostrarNombre(nombre);
+        cout<<"Numero de rondas  "<< totalrondas <<endl;
+            //PRUEBA
+            if(tirarUnDado() > 1) {
+                for(int i = 0; i < dadosDisponibles; i++){
+                    dadosJuego[i] = 3;
+                }
+            } else {
+                tirarNDados(dadosJuego,dadosDisponibles); //codigo original
+            }
+            //FIN PRUEBA
 
-tiradaDeDados(dadosBloqueadores,2);
+        tirada = tirada + 1;
+        cout<<"DADOS BLOQUEADORES"<<endl;
+        mostrarDados(dadosBloqueadores,2);
+        cout<<"TIRADA DE DADOS NUMERO "<< tirada <<endl;
+        mostrarDados(dadosJuego,dadosDisponibles);
 
+        if (dadosDisponibles==0){
+            seguirTirando = false;
+        }
 
- while(can!=0 && bandera ){
+        //DETECTA SI LOS DADOS SON IGUALES PARA DUPLICAR DADOS
+        if (dadosDisponibles > 1 && sonTodosIguales(dadosJuego,dadosDisponibles)) {
 
-mostrarNombre(nombre);
-cout<<"Numero de rondas  "<<totalrondas<<endl;
-tiradaDeDados(dadosJuego,can);
+            resultado[totalrondas-1] = resultado[totalrondas-1] + calcularPuntaje(dadosJuego,dadosBloqueadores,dadosDisponibles) * 2;
+            cout<<"Puntaje ronda  " << totalrondas << endl;
+            mostrarPuntajeDeRonda(resultado,totalrondas);
+            puntajeTotal(resultado,totalrondas);
+            puntaje=resultado[totalrondas-1];
+            system("pause");
+            
+            seguirTirando = true;
+            
+        } else {
 
-tirada = tirada + 1;
-cout<<"DADOS BLOQUEADORES"<<endl;
-mostrarDados(dadosBloqueadores,2);
-cout<<"TIRADA DE DADOS NUMERO "<< tirada <<endl;
-mostrarDados(dadosJuego,can);
+            resultado[totalrondas-1]= resultado[totalrondas-1] + calcularPuntaje(dadosJuego,dadosBloqueadores,dadosDisponibles);
 
-if (can==0){
-    bandera = false;
-}
+            finDelaPartida(resultado,totalrondas,dadosDisponibles);
 
-//DETECTA SI LOS DADOS SON IGUALES PARA DUPLICAR DADOS
-if (sonTodosIguales(dadosJuego,can)&& can>1) {
+            cout<<"Puntaje ronda  "<<totalrondas<<endl;
+            mostrarPuntajeDeRonda(resultado,totalrondas);
+            puntajeTotal(resultado,totalrondas);
+            puntaje = resultado[totalrondas-1];
 
- resultado[totalrondas-1]= resultado[totalrondas-1] + calcularPuntaje(dadosJuego,dadosBloqueadores,can) * 2;
-    finDelaPartida(resultado,totalrondas,can);
-    cout<<"Puntaje ronda  "<<totalrondas<<endl;
-    mostratPuntajeDeRonda(resultado,totalrondas);
-    puntajeTotal(resultado,totalrondas);
-    puntaje=resultado[totalrondas-1];
+            cout<<"Si quiere seguir tirando presione 1 o si quiero terminar la ronda presion 2"<<endl;
+            cin>>seguirTirando;
 
-  while(bandera!= 1){
-    cout<<"Debe volver a tirar PRESIONE 1"<<endl;
-    cin>>bandera;}
+            if (seguirTirando == 2){
+                seguirTirando = false;
 
+            }
 
-    bandera = true;
-}
+        }
 
+        system("cls");
 
-else {
-   resultado[totalrondas-1]= resultado[totalrondas-1] + calcularPuntaje(dadosJuego,dadosBloqueadores,can)  ;
-
-    finDelaPartida(resultado,totalrondas,can);
-    cout<<"Puntaje ronda  "<<totalrondas<<endl;
-    mostratPuntajeDeRonda(resultado,totalrondas);
-    puntajeTotal(resultado,totalrondas);
-    puntaje=resultado[totalrondas-1];
-
-
-cout<<"Si quiere seguir tirando presione 1 o si quiero terminar la ronda presion 2"<<endl;
-cin>>bandera;
-if (bandera == 2){
-    bandera = false;
-
-
-}
-
-}
-
- system("cls");
-
- }
-
-
+    }
 
 
 
-return puntaje;
+
+
+    return puntaje;
  }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -245,10 +226,10 @@ return puntaje;
 
 string pedirNombre(string &name) {
 
- cout<<"Por favor ingresar nombre"<<endl;
- cin>>name;
+    cout<<"Por favor ingresar nombre"<<endl;
+    cin>>name;
 
-return name;
+    return name;
 
 }
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -256,24 +237,20 @@ return name;
 
 void mostrarNombre(string &name){
 
-cout<<"Hola "<<name<<",es timeeee!"<<endl;
+    cout << "Hola " << name << ", es timeeee!" << endl;
 
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-void mostratPuntajeDeRonda(int vec[],int cantidad) {
+void mostrarPuntajeDeRonda(int vec[],int cantidad) {
 
-int i;
+    for (int i=0;i<cantidad;i++){
 
+        cout<<"Round "<<i+1<<" :"<<vec[i]<<endl;
 
-for (i=0;i<cantidad;i++){
-
-cout<<"Round "<<i+1<<" :"<<vec[i]<<endl;
-
-}
-
+    }
 
 }
 
@@ -281,33 +258,29 @@ cout<<"Round "<<i+1<<" :"<<vec[i]<<endl;
 
 void puntajeTotal(int vec[],int cantidad) {
 
-int i,resultado=0;
+    int resultado=0;
 
+    for (int i=0;i<cantidad;i++){
 
+        resultado = resultado + vec[i];
 
-for (i=0;i<cantidad;i++){
+    }
 
-    resultado = resultado + vec[i];
-
-}
-
-cout<<"Puntaje total "<<resultado<<endl;
+    cout<<"Puntaje total "<<resultado<<endl;
 
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 void finDelaPartida(int dadosPuntajes[],int cantidadDeRondas, int cantidadDeDados){
- int i;
+ 
+    if (cantidadDeDados == 0 ) {
 
+        dadosPuntajes[cantidadDeRondas-1] = 0;
 
-  if (cantidadDeDados ==0 ) {
+    }
 
-    dadosPuntajes[cantidadDeRondas-1]=0;
-
-  }
-
-  }
+}
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -325,28 +298,26 @@ void finDelaPartida(int dadosPuntajes[],int cantidadDeRondas, int cantidadDeDado
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
   void puntajeparcial(int lista1[],int lista2[], int cantidad,string nombre1,string nombre2){
-  int acum1=0;
-  int acum2=0;
+    int acum1 = 0;
+    int acum2 = 0;
 
-  cout<<nombre1<<": ";
-  for(int i=0; i<cantidad; i++)
-  {
-    cout << " " << lista1[i];
-    acum1+=lista1[i];
+    cout << nombre1 << ": ";
 
-  }
-  cout<<" = "<<acum1;
-  cout << endl;
+    for(int i = 0; i < cantidad; i++){
+        cout << " " << lista1[i];
+        acum1+= lista1[i];
+    }
 
- if (nombre2!= "0"){
-  cout<<nombre2<<": ";
-   for(int i=0; i<cantidad; i++)
-  {
-    cout << " " << lista2[i];
-    acum2+=lista2[i];
+    cout << " = " << acum1;
+    cout << endl;
 
-  }
-  cout<<" = "<<acum2;
-  cout << endl;
-  }
+    if (nombre2 != "0"){
+        cout << nombre2 << ": ";
+        for(int i = 0; i < cantidad; i++){
+            cout << " " << lista2[i];
+            acum2+= lista2[i];
+        }
+        cout<<" = "<<acum2;
+        cout << endl;
+    }
 }
